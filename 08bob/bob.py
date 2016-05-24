0c2ae54247070b29a22debdcce594edfc0b65c05
Ginny <ginny.pennekamp@gmail.com>
2016-05-24 12:50:56 -0700

    Fixed bob third time

--- 08bob/bob.py
+++ 08bob/bob.py
@@ -1,20 +1,14 @@
 #
 # Skeleton file for the Python "Bob" exercise.
 #
-
-
 def hey(what):
     what = what.strip()
-    what = what.replace(",", "")
-    what = what.replace(" ", "")
 
-    if what:
-        if what.isupper():
-            return 'Whoa, chill out!'
-        elif what[-1:] == "?":
-            return 'Sure.'
-        else:
-            return 'Whatever.'
+    if what.isupper():
+        return 'Whoa, chill out!'
+    elif what[-1:] == "?":
+        return 'Sure.'
+    elif what:
+        return 'Whatever.'
     else:
         return 'Fine. Be that way!'
-
