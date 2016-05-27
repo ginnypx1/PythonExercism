e5730864bb5c4f3f4d2f13ef1ec3f842f590a4f6
Ginny <ginny.pennekamp@gmail.com>
2016-05-27 11:26:11 -0700

    Second iteration series

--- 14series/series.py
+++ 14series/series.py
@@ -1,18 +1,16 @@
 def slices(numstring, slice_value):
     """Chops a given string of numbers into all possible consecuctive slices of the given length"""
-    i = 0
-    slice_list = []
-    while slice_value <= len(numstring):
-        # grab a string slice and turn it into an array of ints
-        intlist = list(map(lambda x: int(x), numstring[i:slice_value]))
-        # if it's not empty, append it to results lists
-        if intlist:
-            slice_list.append(intlist)
-        # increase the variables
-        i += 1
-        slice_value += 1
-    # if there is a results list, return it, or raise a value error
-    if slice_list:
-        return slice_list
-    else:
+    if (slice_value == 0) or (slice_value > len(numstring)):
         raise ValueError("Length argument doesn't fit the series.")
+
+    slice_list = []
+    # convert string to list of numbers
+    intlist = [int(num) for num in numstring]
+    # find subsets
+    for i in range(0, (len(numstring)-slice_value+1)):
+        slice_list.append(intlist[i: i+slice_value])
+    return slice_list
+
+if __name__ == '__main__':
+    print(slices("97867564", 2)) 
+    print(slices("97867564", 3)) 
